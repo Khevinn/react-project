9981aacdaa52689aa43954277dec5b8aba3bc4f4
Khevinn <Karimoxiha@gmail.com>
2020-10-24 00:00:42 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require( 'path' )
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' )
+
+module.exports = {
+  entry: path.resolve( 'src', 'index.jsx' ),
+  output: {
+    path: path.resolve( 'dist' ),
+  },
+  module: {
+    rules: [ {
+      test: /\.jsx?$/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.s[ac]ss$/,
+      use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+    } ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin( {
+      template: path.resolve( 'index.html' )
+    } ),
+    new MiniCssExtractPlugin( {} )
+  ],
+  resolve: {
+    extensions: [ '.js', '.json', '.jsx' ]
+  }
+}
